d4d38df66e3adb9281b007b6c76c12cddb71f2c2
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2014-07-01 13:57:53 +0200

    Created solution

---
+++ GeneratorClient/GeneratorServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
