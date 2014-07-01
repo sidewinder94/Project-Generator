26f3f6a43c021eeb1bd1a0aa1963626af57c9fdd
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2014-07-01 14:49:39 +0200

    Added Contracts

---
+++ GeneratorContracts/DataContracts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace GeneratorServiceContracts
+{
+    [DataContract]
+    public enum Operations
+    {
+        [EnumMember]
+        Authenticate = 0,
+        [EnumMember]
+        Decode = 1
+    }
+
+    public enum Status
+    {
+        [EnumMember]
+        Sent = 0,
+        [EnumMember]
+        Suceeded = 1,
+        [EnumMember]
+        Failed = 2
+    }
+
+    [DataContract]
+    class Message
+    {
+        [DataMember]
+        public String ApplicationToken { get; set; }
+        [DataMember]
+        public object[] Data { get; set; }
+        [DataMember]
+        public String Info { get; set; }
+        [DataMember]
+        public Operations Operation { get; set; }
+        [DataMember]
+        public Status Status { get; set; }
+        [DataMember]
+        public String UserToken { get; set; }
+    }
+}
---
+++ GeneratorContracts/IWorkService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace GeneratorServiceContracts
+{
+    public interface ICallBackClient
+    {
+        [OperationContract(IsOneWay = true)]
+        void NotifyClient(Message msg);
+    }
+
+
+    [ServiceContract(CallbackContract = typeof(ICallBackClient))]
+    interface IWorkService
+    {
+        [OperationContract(IsOneWay = true)]
+        void CompletedOperation(Message msg);
+        [OperationContract]
+        Message Service(Message msg);
+        [OperationContract(IsOneWay = true)]
+        void Subscribe(String userToken);
+        [OperationContract(IsOneWay = true)]
+        void Unsubscribe(String userToken);
+    }
+}
