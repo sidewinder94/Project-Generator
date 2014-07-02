c9e269f2e072d46ac648db16dfbbac4eeaa1dbaa
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2014-07-02 08:44:45 +0200

    Added service files

---
+++ GeneratorClient/GeneratorService/CallBackService.cs
@@ -0,0 +1,27 @@
+﻿using GeneratorServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorService
+{
+    public class CallBackService : ICallBackService
+    {
+        public void Subscribe(string userToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe(string userToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyClient(Message msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
